367ec9f6bdf8b694a17758c004d880775795e63c
RhymesR <allmicandfame@gmail.com>
2026-06-11 18:23:02 +0100

    update ui for hero section

--- app/casual/Hero.tsx
+++ app/casual/Hero.tsx
@@ -13,9 +13,9 @@ const Hero = () => {
 					<span className='bg-tertiary/10 text-tertiary px-4 py-1 rounded-full font-label-sm text-label-sm inline-block mb-6'>NEW COLLECTION</span>
 					<div className='w-70 md:w-full'>
 						<h1 className='md:font-display-lg font-bold sm:text-display-lg text-xl mb-4 text-red-800'>Casual Chic</h1>
-						<p className='sm:flex sm:font-body-lg sm:text-body-lg text-sm text-gray-700 mb-8 leading-relaxed'>Where relaxed comfort meets ancestral artistry. Discover modern separates and effortless tunics designed for the contemporary lifestyle, featuring subtle West African textile motifs.</p>
+						<p className='sm:flex sm:font-body-lg sm:text-body-lg text-[14px] text-gray-700 mb-8 leading-relaxed'>Where relaxed comfort meets ancestral artistry. Discover modern separates and effortless tunics designed for the contemporary lifestyle, featuring subtle West African textile motifs.</p>
 					</div>
-					<div className='flex sm:gap-4 gap-16 mb-4'>
+					<div className='flex flex-col md:flex-row  md:gap-4 gap-6 mb-4 '>
 						<button
 							onClick={() => {
 								const el = document.getElementById('collection')
@@ -23,7 +23,7 @@ const Hero = () => {
 							}
 						}
 							className='shimmer-btn text-gray-600 hover:text-white px-4 py-2 sm:px-8 sm:py-4 rounded-full hover:bg-primary font-title-md text-title-md cursor-pointer shadow-lg transition-transform active:scale-95'>	Shop Collection</button>
-						<button className='glass-panel hover:scale-110 duration-700 px-4 py-2  sm:px-8 sm:py-4 rounded-full  font-title-md text-title-md text-on-surface shadow-lg active:scale-95 cursor-pointer hover:bg-white transition-all'>View Lookbook</button>
+						<button className='glass-panel hover:scale-105 duration-700 px-4 py-2  sm:px-8 sm:py-4 rounded-full  font-title-md text-title-md text-on-surface shadow-lg active:scale-95 cursor-pointer hover:bg-white transition-all'>Lookbook</button>
 					</div>
 				</div>
 			</div>
